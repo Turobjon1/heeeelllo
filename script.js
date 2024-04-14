430a8235dba8abd73e33e58ae18b201a92b9faf4
Turobjon <avezovzamir337@gmail.com>
2024-04-14 08:05:55 +0500

    hello

---
+++ script.js
@@ -0,0 +1,10 @@
+let ism = prompt('ism kirit bolajon ')
+
+let harf = prompt('harf kirit bolajon ')
+
+if (ism.includes(harf)){
+    alert( `${ism} ichida ${harf} mavjud   ` )
+}
+else{
+    alert( `${ism} ichida ${harf} mavjud emas  ` )
+}
